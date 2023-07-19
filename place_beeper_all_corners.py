64c1a2d7a9875ccc936dbd819be618b8636ba579
Hacker1659 <139866751+Hacker1659@users.noreply.github.com>
2023-07-19 09:56:04 +0100

    Fixed bugs on place beeper

---
+++ place_beeper_all_corners.py
@@ -0,0 +1,34 @@
+from stanfordkarel import *
+def main():
+  place_beepers_everywhere()
+
+def turn_around():
+  turn_left()
+  turn_left()
+
+def place_beepers_row():
+  turn_left()
+  while front_is_clear():
+    if not beepers_present():
+      put_beeper()
+    move()
+  if not beepers_present():
+    put_beeper() 
+
+def place_beepers_everywhere():
+  while front_is_clear():  
+      place_beepers_row()
+      go_back()
+      move()
+  place_beepers_row()
+
+def go_back():
+  turn_around()
+  while front_is_clear():
+    move()
+  turn_left()
+
+if __name__ == "__main__":
+    run_karel_program("sample_quad1")
+   
+   
